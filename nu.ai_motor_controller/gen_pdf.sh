fbbc0dd2b419dbe8bf6c0028c4697d620cbe9887
DJ <djd9617@rit.edu>
2019-02-21 15:21:52 -0500

    automated commit

---
+++ nu.ai_motor_controller/gen_pdf.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+pdfunite \
+nu.ai_motor_controller-Edge.Cuts.pdf \
+nu.ai_motor_controller-B.SilkS.pdf \
+nu.ai_motor_controller-F.SilkS.pdf \
+nu.ai_motor_controller-F.Paste.pdf \
+nu.ai_motor_controller-F.Mask.pdf \
+nu.ai_motor_controller-F.Cu.pdf \
+nu.ai_motor_controller-In1.Cu.pdf \
+nu.ai_motor_controller-In2.Cu.pdf \
+nu.ai_motor_controller-B.Cu.pdf \
+nu.ai_motor_controller-B.Mask.pdf \
+nu.ai_motor_controller-B.Paste.pdf \
+layers.pdf
